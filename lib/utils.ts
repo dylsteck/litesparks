12401414a157da0638a6660bab784dbd09e3b567
dylsteck <dylancsteck@gmail.com>
2024-09-12 01:22:41 -0400

    fix: change LITESPARKS_API for preview envt

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,8 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-export const LITESPARKS_API = "http://localhost:3000";
-// export const LITESPARKS_API = "https://litesparks.xyz";
+//export const LITESPARKS_API = "http://localhost:3000";
+export const LITESPARKS_API = "https://litesparks.xyz";
 export const SKELETON_PFP_URL = "https://i.imgur.com/cey3rmT.png";
 export const ZORA_CLIENT_API = "https://zora.co/api";
 export const ZORA_BACKEND_API = "https://api.zora.co";
